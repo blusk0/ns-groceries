9b14e26e635c8ac0f9b2cbcfeec7824c758622fd
Lusk, Brandon T <btlusk@geisinger.edu>
2018-06-11 06:51:37 -0400

    Hint functionality

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-import { User } from '../../shared/user/user';
-import { UserService } from '../../shared/user/user.service';
-import { Router } from '@angular/router';
-import { Page } from 'ui/page';
-import { Color } from 'color';
-import { View } from 'ui/core/view';
+import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
+import { User } from "../../shared/user/user";
+import { UserService } from "../../shared/user/user.service";
+import { Router } from "@angular/router";
+import { Page } from "ui/page";
+import { Color } from "color";
+import { View } from "ui/core/view";
+import { setHintColor } from "../../utils/hint-util";
+import { TextField } from "ui/text-field";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './pages/login/login.html',
-  styleUrls: ['./pages/login/login-common.css', './pages/login/login.css']
+  selector: "app-login",
+  templateUrl: "./pages/login/login.html",
+  styleUrls: ["./pages/login/login-common.css", "./pages/login/login.css"]
 })
 export class LoginComponent implements OnInit {
   user: User;
   isLoggingIn = true;
-  @ViewChild('container') container: ElementRef;
+  @ViewChild("container") container: ElementRef;
+  @ViewChild("email") email: ElementRef;
+  @ViewChild("password") password: ElementRef;
 
   constructor(
     private router: Router,
@@ -22,18 +26,18 @@ export class LoginComponent implements OnInit {
     private page: Page
   ) {
     this.user = new User();
-    this.user.email = 'blusk05@gmail.com';
-    this.user.password = 'testpassword';
+    this.user.email = "blusk05@gmail.com";
+    this.user.password = "testpassword";
   }
 
   ngOnInit() {
     this.page.actionBarHidden = true;
-    this.page.backgroundImage = 'res://bg_login';
+    this.page.backgroundImage = "res://bg_login";
   }
 
   submit() {
     if (!this.user.isValidEmail()) {
-      alert('Enter a valid email address.');
+      alert("Enter a valid email address.");
       return;
     }
     if (this.isLoggingIn) {
@@ -47,28 +51,43 @@ export class LoginComponent implements OnInit {
     this.userService
       .login(this.user)
       .subscribe(
-        () => this.router.navigate(['/list']),
-        error => alert('Unfortunately we could not find your account.')
+        () => this.router.navigate(["/list"]),
+        error => alert("Unfortunately we could not find your account.")
       );
   }
 
   signUp() {
     this.userService.register(this.user).subscribe(
       () => {
-        alert('Your account was successfully created.');
+        alert("Your account was successfully created.");
         this.toggleDisplay();
       },
-      () => alert('Unfortunately we were unable to create your account.')
+      () => alert("Unfortunately we were unable to create your account.")
     );
   }
+
   toggleDisplay() {
     this.isLoggingIn = !this.isLoggingIn;
+    this.setTextFieldColors();
     let container = <View>this.container.nativeElement;
     container.animate({
       backgroundColor: this.isLoggingIn
-        ? new Color('white')
-        : new Color('#301217'),
+        ? new Color("white")
+        : new Color("#301217"),
       duration: 200
     });
+  }
+
+  setTextFieldColors() {
+    let emailTextField = <TextField>this.email.nativeElement;
+    let passwordTextField = <TextField>this.password.nativeElement;
+  
+    let mainTextColor = new Color(this.isLoggingIn ? "black" : "#C4AFB4");
+    emailTextField.color = mainTextColor;
+    passwordTextField.color = mainTextColor;
+  
+    let hintColor = new Color(this.isLoggingIn ? "#ACA6A7" : "#C4AFB4");
+    setHintColor({ view: emailTextField, color: hintColor });
+    setHintColor({ view: passwordTextField, color: hintColor });
   }
 }
